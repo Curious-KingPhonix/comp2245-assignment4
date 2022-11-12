c0965ad27f01e584ac401cc00fa2d21578239e27
KlonedBorn <michealdaivd132@gmail.com>
2022-11-12 16:16:39 -0400

    + Finished

--- app.js
+++ app.js
@@ -12,31 +12,32 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 const httpRequest = new XMLHttpRequest()
+const url = "superheroes.php"
 
-function makeHttpRequest(url){
+function makeHttpRequest(query){
     if(!httpRequest){
         alert("Error | Unable to create XMLHttpRequest object")
         return false;
     }
+
     httpRequest.onreadystatechange = () =>{
         if( httpRequest.readyState === XMLHttpRequest.DONE){
             if( httpRequest.status === 200 ) {
                 // Do something
                 document.getElementById("search-result").innerHTML = httpRequest.responseText
             } else {
-                alert(`XMLHttpRequest returned ${httpRequest,status}`)
+                alert(`XMLHttpRequest returned ${httpRequest.status}`)
             }
         }
     }
-    httpRequest.open("GET",url)
+    httpRequest.open('GET',`superheroes.php?query=${query}`,true)
     httpRequest.send()
 }
 
 window.onload = () => {
     document.getElementById('submit').onclick = (e) => {
         let query = document.getElementById('search-index').value
-        queryUrl = `superheroes.php?query=${query}`
-        makeHttpRequest(queryUrl)
+        makeHttpRequest(query)
     }
 }
 window
